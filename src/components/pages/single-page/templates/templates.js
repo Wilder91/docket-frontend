9e5cf4e2025fa39539cd5c775db99318e2f2cc71
Michael Wilder <mikewilder01@gmail.com>
2023-10-31 11:52:11 -0400

    progress on templates screen

--- src/components/pages/single-page/templates/templates.js
+++ src/components/pages/single-page/templates/templates.js
@@ -24,30 +24,37 @@ function TemplateIndex({ templates, setTemplates, user }) {
 
   return (
     <Card className="template-list">
-      <div id='template.id' className="template-container">
-        <h1 className="template-headline">My Templates</h1>
-        <br />
-        {templates.map((template) => (
-          <div key={template.id} className="template-info">
-            <h1 className="project-names">{template.name}</h1>
+    <div id="template.id" className="Card">
+      <h1 className="template-headline">My Templates</h1>
+      <br />
+      {templates.map((template) => (
+        <div key={template.id} className="template-info">
+          <h1 className="project-names">{template.name}</h1>
+          <br/>
+          <div className="milestones-container">
             {template.milestones
               .sort((a, b) => new Date(b.due_date) - new Date(a.due_date))
               .map((milestone, index) => (
                 <Card key={milestone.id} className="milestone-card">
                   <div className="milestone-info">
-                    <p>{index + 1} {milestone.name} {milestone.leadTime} Days Before Due Date</p>
+                    <p>
+                      {index + 1} {milestone.name} {milestone.leadTime} Days Before Due Date
+                    </p>
                   </div>
                   <br />
                 </Card>
               ))}
-            <button className="normal" onClick={() => removeTemplate(template.id)}>
-              Delete
-            </button>{" "}
           </div>
-        ))}
-        <br />
-      </div>
-    </Card>
+          <button className="normal" onClick={() => removeTemplate(template.id)}>
+            Delete
+          </button>{" "}
+        </div>
+      ))}
+      <br />
+    </div>
+  </Card>
+  
+
   );
   
 }
