0856dabb1483efef5f5a100dd6e493a58002b5c7
Michael Wilder <mikewilder01@gmail.com>
2024-01-12 16:10:14 -0500

    Updates

---
+++ precache-manifest.416c211a43ea8454b2d0f3f2105dc85d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0162aea0c35a51d8004",
+    "url": "/docket/static/css/main.bdf14430.chunk.css"
+  },
+  {
+    "revision": "c0162aea0c35a51d8004",
+    "url": "/docket/static/js/main.1912a0cc.chunk.js"
+  },
+  {
+    "revision": "59fba937ced2abe55eef",
+    "url": "/docket/static/js/runtime~main.94fe04e1.js"
+  },
+  {
+    "revision": "14efa3a1e6be41317836",
+    "url": "/docket/static/css/2.2c035b1e.chunk.css"
+  },
+  {
+    "revision": "14efa3a1e6be41317836",
+    "url": "/docket/static/js/2.218eb5e9.chunk.js"
+  },
+  {
+    "revision": "a9e0708893377349507b",
+    "url": "/docket/static/js/3.a0d46aea.chunk.js"
+  },
+  {
+    "revision": "a5ca7c5f46dfddff52d85eee7de617c9",
+    "url": "/docket/static/media/NonUrgent.a5ca7c5f.png"
+  },
+  {
+    "revision": "8f8c505a50f877004c7dcddc6b01cf3d",
+    "url": "/docket/static/media/Overdue.8f8c505a.png"
+  },
+  {
+    "revision": "936d036e37cd7817f2b8cbbb888800a3",
+    "url": "/docket/static/media/Upcoming.936d036e.png"
+  },
+  {
+    "revision": "6addf8f7cad823b71bf2c4fcafc10030",
+    "url": "/docket/static/media/MediumUrgent.6addf8f7.png"
+  },
+  {
+    "revision": "8563696dde5279cf2d1cf00f2567195a",
+    "url": "/docket/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.d916dcb012b4ef0b82eb825a226b5712.js"
+  "/docket/precache-manifest.416c211a43ea8454b2d0f3f2105dc85d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/docket/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
