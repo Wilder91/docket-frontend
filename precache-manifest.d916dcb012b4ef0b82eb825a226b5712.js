4d442f793ce57405623e6f8995ac686cad82a4ad
Michael Wilder <mikewilder01@gmail.com>
2024-01-12 15:51:09 -0500

    Updates

---
+++ precache-manifest.d916dcb012b4ef0b82eb825a226b5712.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0162aea0c35a51d8004",
+    "url": "/myapp/static/css/main.bdf14430.chunk.css"
+  },
+  {
+    "revision": "c0162aea0c35a51d8004",
+    "url": "/myapp/static/js/main.1912a0cc.chunk.js"
+  },
+  {
+    "revision": "ac3a4a368860545e824a",
+    "url": "/myapp/static/js/runtime~main.3252c6b3.js"
+  },
+  {
+    "revision": "14efa3a1e6be41317836",
+    "url": "/myapp/static/css/2.2c035b1e.chunk.css"
+  },
+  {
+    "revision": "14efa3a1e6be41317836",
+    "url": "/myapp/static/js/2.218eb5e9.chunk.js"
+  },
+  {
+    "revision": "a9e0708893377349507b",
+    "url": "/myapp/static/js/3.a0d46aea.chunk.js"
+  },
+  {
+    "revision": "a5ca7c5f46dfddff52d85eee7de617c9",
+    "url": "/myapp/static/media/NonUrgent.a5ca7c5f.png"
+  },
+  {
+    "revision": "8f8c505a50f877004c7dcddc6b01cf3d",
+    "url": "/myapp/static/media/Overdue.8f8c505a.png"
+  },
+  {
+    "revision": "936d036e37cd7817f2b8cbbb888800a3",
+    "url": "/myapp/static/media/Upcoming.936d036e.png"
+  },
+  {
+    "revision": "6addf8f7cad823b71bf2c4fcafc10030",
+    "url": "/myapp/static/media/MediumUrgent.6addf8f7.png"
+  },
+  {
+    "revision": "61658723253038b88c4ba883281af2dd",
+    "url": "/myapp/index.html"
+  }
+];
